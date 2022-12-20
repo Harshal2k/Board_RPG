cafac2554aa1dcd66ee020f12f5f749f06f8c7c7
User <User@DESKTOP-DGTOP58.mshome.net>
2022-12-20 12:17:56 +0530

    Added class Witcher

---
+++ src/Character/Witcher.java
@@ -0,0 +1,8 @@
+package Character;
+
+public class Witcher extends GameCharacter {
+
+public Witcher(String name,Integer Health,Integer posX,Integer posY) {
+super(name,Health, posX, posY,100,45,3);
+}
+}
