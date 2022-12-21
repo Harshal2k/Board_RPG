076d30581ee460722d23f1d2a153799c37fb2ad5
2202-AryanK <aryankushwaha19@gmail.com>
2022-12-21 11:43:23 +0530

    Added Update character method and modified the player constructor to set king character.

--- src/Game/Player.java
+++ src/Game/Player.java
@@ -1,13 +1,18 @@
 package Game;
 
 import Character.GameCharacter;
+import Character.King;
 
 public class Player {
 	private String name, house = "";
 	private GameCharacter characters[] = new GameCharacter[4];
 
-	public Player() {
-	
+	public Player(Integer playerNo) {
+		if (playerNo == 1) {
+			characters[0] = new King("P1K", 150, 2, 0);
+		} else if (playerNo == 2) {
+			characters[0] = new King("P2K", 150,2,5);
+		}
 	}
 
 	public String getName() {
@@ -32,5 +37,12 @@ public class Player {
 
 	public void setCharacters(GameCharacter[] characters) {
 		this.characters = characters;
+	}
+	
+	
+	public void updateCharacter(GameCharacter character, Integer position) {
+		if (position > 0 && position < 4) {
+			characters[position]=character;
+		}
 	}
 }
